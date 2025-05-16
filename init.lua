f40e7ab7a18b4053fe7ed7f72cf9b70b0f6c57fb
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2025-05-16 23:55:34 +0530

    Add lazy.nvim package manager

--- init.lua
+++ init.lua
@@ -1,1 +1,18 @@
 require("core")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+
+---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({})

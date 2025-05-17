158e5564df12368e2e4b731a70a924fa4193d89c
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2025-05-17 12:35:08 +0530

    Add treesitter

--- init.lua
+++ init.lua
@@ -2,17 +2,24 @@ require("core")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 
 ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({})
+-- Set up lazy, and load the required folders
+require("lazy").setup({
+  { import = "plugins.treesitter" },
+}, {
+  change_detection = {
+    notify = false,
+  },
+})

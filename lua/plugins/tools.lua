cb500a359dd75cfb5da2be2bf5a07efd42865514
Sayantan C. <syntnc@proton.me>
2025-05-26 18:38:16 +0530

    Add persistence as session manager

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -17,4 +17,18 @@ return {
       { "-", "<cmd>Oil --float<CR>", silent = true, desc = "Open folder in Oil" },
     },
   },
+
+  -- Session management
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = vim.opt.sessionoptions:get() },
+    -- stylua: ignore
+    keys = {
+      { "<leader>sf", function() require("persistence").select() end, desc = "Find Session" },
+      { "<leader>ss", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>sl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>sd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    },
+  },
 }

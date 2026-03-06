9cbad13de62d2e6dc342607131c5a327ea9e9811
Sayantan C. <syntnc@proton.me>
2026-03-06 19:25:14 +0530

    Refactor statusline utils

--- lua/utils/statusline.lua
+++ lua/utils/statusline.lua
@@ -1,53 +1,37 @@
 local M = {}
 
 M.components = {
-  -- harpoon = {
-  --   function()
-  --     local harpoon = require("harpoon.mark")
-  --     local total_marks = harpoon.get_marks()
-  --     if total_marks == 0 then
-  --       return ""
-  --     end
-  --     local current_mark = "-"
-  --     local mark_idx = harpoon.get_marks()
-  --     if mark_idx ~= nil then
-  --       current_mark = tostring(mark_idx)
-  --     end
-  --     return string.format("[%d/%s]", current_mark, total_marks)
-  --   end,
-  --   icon = "󰛢 ",
-  -- },
   lsp_info = {
     function()
-      local msg = "No Active LSP"
-      local buf_ft = vim.bo.filetype
-      local clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
-      if next(clients) == nil then
-        return msg
+      local clients = vim.lsp.get_clients({ bufnr = 0 })
+      if #clients == 0 then
+        return "No Active LSP"
       end
-      for _, client in ipairs(clients) do
-        local filetypes = client.config.filetypes
-        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-          return client.name
-        end
-      end
-      return msg
+      return table.concat(
+        vim
+          .iter(clients)
+          :map(function(client)
+            return client.name
+          end)
+          :totable(),
+        ", "
+      )
     end,
     icon = " ",
     color = { bg = "base" },
   },
   tab_indicator = {
     function()
-      local tabline = {}
-      local cur_tab = vim.api.nvim_get_current_tabpage()
-      for _, tab_id in ipairs(vim.api.nvim_list_tabpages()) do
-        if tab_id == cur_tab then
-          table.insert(tabline, string.format("[%s]", tab_id))
-        else
-          table.insert(tabline, tab_id)
-        end
-      end
-      return table.concat(tabline, " ")
+      local cur = vim.api.nvim_get_current_tabpage()
+      return table.concat(
+        vim
+          .iter(vim.api.nvim_list_tabpages())
+          :map(function(id)
+            return id == cur and string.format("[%s]", id) or tostring(id)
+          end)
+          :totable(),
+        " "
+      )
     end,
     icon = " ",
     color = "MoreMsg",

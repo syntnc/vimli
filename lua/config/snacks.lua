f106577191129060d793616af6ab04f6d2cbf41a
Sayantan C. <syntnc@proton.me>
2026-07-20 19:22:58 +0530

    Add snacks picker

---
+++ lua/config/snacks.lua
@@ -0,0 +1,80 @@
+local icons = require("utils.icons")
+
+--- @type snacks.Config
+return {
+  bigfile = {
+    enabled = true,
+    notify = true,
+    size = 100 * 1024, -- 50 KB
+  },
+  bufdelete = { enabled = true },
+  git = { enabled = true },
+  gitbrowse = { enabled = true },
+  indent = {
+    enabled = true,
+    priority = 1,
+    char = "│",
+    only_scope = false,
+    only_current = false,
+    hl = {
+      "SnacksIndent1",
+      "SnacksIndent2",
+      "SnacksIndent3",
+      "SnacksIndent4",
+      "SnacksIndent5",
+      "SnacksIndent6",
+      "SnacksIndent7",
+      "SnacksIndent8",
+    },
+  },
+  input = {
+    enabled = true,
+    icon = icons.ui.edit,
+    icon_hl = "SnacksInputIcon",
+    icon_pos = "left",
+    prompt_pos = "title",
+    win = { style = "input" },
+    expand = true,
+  },
+  notifier = {
+    enabled = true,
+    timeout = 2000,
+    width = { min = 40, max = 0.4 },
+    height = { min = 1, max = 0.6 },
+    margin = { top = 0, right = 1, bottom = 0 },
+    padding = true,
+    sort = { "level", "added" },
+    level = vim.log.levels.TRACE,
+    icons = {
+      debug = icons.ui.bug,
+      error = icons.diagnostics.Error,
+      info = icons.diagnostics.Info,
+      trace = icons.ui.bookmark,
+      warn = icons.diagnostics.Warn,
+    },
+    style = "compact",
+    top_down = true,
+    date_format = "%R",
+    more_format = "... %d lines",
+    refresh = 50,
+  },
+  notify = { enabled = true },
+  picker = { enabled = vim.g.picker == "snacks" },
+  profiler = { enabled = true },
+  quickfile = { enabled = true },
+  rename = { enabled = true },
+  scroll = { enabled = false },
+  statuscolumn = {
+    enabled = true,
+    left = { "mark", "sign" },
+    right = { "fold", "git" },
+    folds = {
+      open = false,
+      git_hl = false,
+    },
+    git = {
+      patterns = { "GitSign", "MiniDiffSign" },
+    },
+    refresh = 50,
+  },
+}
---
+++ plugin/snacks.lua
@@ -0,0 +1,25 @@
+vim.pack.add({
+  { src = plug("folke/snacks.nvim") },
+})
+
+require("snacks").setup(require("config.snacks"))
+
+if vim.g.picker == "snacks" then
+  local map = vim.keymap.set
+  local picker = require("snacks").picker
+  map("n", "<leader>fb", function() picker.buffers() end, { desc = "Buffers" })
+  map("n", "<leader>fc", function() picker.files({ cwd = vim.fn.stdpath("config") }) end, { desc = "Config File" })
+  map("n", "<leader>fd", function() picker.diagnostics_buffer() end, { desc = "Diagnostics in Buffer" })
+  map("n", "<leader>fD", function() picker.diagnostics() end, { desc = "Diagnostics" })
+  map("n", "<leader>ff", function() picker.files() end, { desc = "Files" })
+  map("n", "<leader>fg", function() picker.git_files() end, { desc = "Git Files" })
+  map("n", "<leader>fh", function() picker.help() end, { desc = "Help Tags" })
+  map("n", "<leader>fi", function() picker.icons() end, { desc = "Icons" })
+  map("n", "<leader>fk", function() picker.keymaps() end, { desc = "Keymaps" })
+  map("n", "<leader>fn", function() picker.notifications() end, { desc = "Notifications" })
+  map("n", "<leader>fp", function() picker.projects() end, { desc = "Projects" })
+  map("n", "<leader>fr", function() picker.recent() end, { desc = "Recent" })
+  map({ "n", "x" }, "<leader>fvw", function() picker.grep_word() end, { desc = "Visual selection or word" })
+  map("n", "<leader>fw", function() picker.grep() end, { desc = "Grep" })
+  map("n", "<leader>f?", function() picker.pickers() end, { desc = "All Pickers" })
+end

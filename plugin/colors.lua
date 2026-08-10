bf8e2d08817f83916085f9c2c6ffa00a9dc3a9a6
Sayantan C. <syntnc@proton.me>
2026-08-10 20:09:04 +0530

    Add cendre colorscheme

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,7 +1,13 @@
 vim.pack.add({
   { src = plug("rose-pine/neovim"), name = "rose-pine" },
+  { src = plug("Aejkatappaja/cendre"), name = "cendre" },
 })
 
 require("rose-pine").setup(require("config.rose-pine"))
+require("cendre").setup({
+  background = "medium", -- "hard" | "medium" | "soft"
+  italic_virtual_text = false,
+  transparent = true,
+})
 
-vim.cmd.colorscheme("rose-pine")
+vim.cmd.colorscheme("cendre")

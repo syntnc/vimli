2de62dcdc9ae7289d6af71c546e4ce61b03e4e26
Sayantan C. <syntnc@proton.me>
2026-08-10 20:09:47 +0530

    Add mini.ai for textobjects

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -2,6 +2,7 @@ vim.pack.add({
   { src = plug("romus204/tree-sitter-manager.nvim") },
   { src = plug("Wansmer/treesj") },
   { src = plug("ckolkey/ts-node-action") },
+  { src = plug("nvim-mini/mini.ai") },
 })
 
 require("tree-sitter-manager").setup({
@@ -10,6 +11,7 @@ require("tree-sitter-manager").setup({
 })
 require("treesj").setup({ use_default_keymaps = false })
 require("ts-node-action").setup()
+require("mini.ai").setup()
 
 vim.keymap.set("n", "<leader>rj", "<cmd>TSJJoin<CR>", { desc = "[J]oin Node" })
 vim.keymap.set("n", "<leader>rs", "<cmd>TSJSplit<CR>", { desc = "[S]plit Node" })
